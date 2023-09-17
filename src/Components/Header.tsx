1b62f64fa3c2c475eacd24715c3ee9e1f1b3e823
Jarv1sGH <itsmejarvis007@gmail.com>
2023-09-17 13:29:18 +0530

    Header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,5 +1,22 @@
+import { AppBar, Typography, Toolbar } from "@mui/material";
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h4" mr={"auto"} textTransform={"uppercase"}>
+          ScottLang
+        </Typography>
+        <Link
+          style={{ color: "White", margin: "0.5rem", textDecoration: "none" }}
+          to={"/"}
+        >
+          Home
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
