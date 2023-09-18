ca2bf9596b19dc5fbec05bf7d925d99aaeff729c
Jarv1sGH <itsmejarvis007@gmail.com>
2023-09-18 21:27:54 +0530

    Completed Learning component

--- src/Components/Learn.tsx
+++ src/Components/Learn.tsx
@@ -1,8 +1,75 @@
+import { Container, Button, Typography, Stack } from "@mui/material";
+import { ArrowBack, VolumeUp } from "@mui/icons-material";
+import { useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Learn = () => {
-  return (
-    <div>Learning</div>
-  )
-}
+  const [count, setCount] = useState<number>(0);
+  // LangType is defined in vite-env.tds
+  const params = useSearchParams()[0].get("language") as LangType;
 
-export default Learn
+  const navigate = useNavigate();
+
+  const nextQuesHandler = (): void => {
+    setCount((prev) => prev + 1);
+  };
+
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        padding: "1rem",
+      }}
+    >
+      <Button
+        onClick={
+          count === 0
+            ? () => {
+                navigate("/");
+              }
+            : () => {
+                setCount((prev) => prev - 1);
+              }
+        }
+      >
+        <ArrowBack />
+      </Button>
+
+      <Typography m={"2rem 0"}>ScottLang your langauage partner</Typography>
+      <Stack direction={"row"} spacing={"1rem"}>
+        <Typography variant="h4">
+          {count + 1} - {"Sample"}
+        </Typography>
+        <Typography color="lightGreen" variant="h4">
+          : {"Sample"}
+        </Typography>
+
+        <Button
+          sx={{
+            borderRadius: "50%",
+          }}
+        >
+          <VolumeUp />
+        </Button>
+      </Stack>
+      <Button
+        sx={{
+          margin: "3rem 0",
+        }}
+        variant="contained"
+        fullWidth
+        onClick={
+          count === 7
+            ? () => {
+                navigate("/quiz");
+              }
+            : nextQuesHandler
+        }
+      >
+        {count === 8 ? "Take test" : "Next"}
+      </Button>
+    </Container>
+  );
+};
+
+export default Learn;
